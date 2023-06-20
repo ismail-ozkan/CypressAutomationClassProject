22ea5be5e0192be0b32a7b5edcc32416cd1a1627
ismailozkanlaw <ismailozkanlaw@gmail.com>
2023-06-20 08:18:42 +0300

    types of assertion

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -4,6 +4,7 @@ describe('Find or Get Elements by Using Different Locators', () => {
         cy.clearCookies();
         cy.visit('/login');
     })
+
     it('Check different locators strategies', () => {
         // By CSS Locator
         cy.get("input[name='username']").type("CydeoStudent");// every statement creates 
@@ -34,4 +35,25 @@ describe('Find or Get Elements by Using Different Locators', () => {
         // a different approach
         cy.get('button').should('contain','Login').click();
     })
+  
+    it('Check finding element by traveling through DOM', () => {
+        // travel to find the login button: locate username box 
+        //go to parent form then find button 
+        cy.get('input[name="username"').parents('form').find('button').should('contain','Login').click();
+    })
+
+    it.only('Check Different Type of Assertions', () => {
+        // Cypress itself bundles assetions provided by Chai, Sinon, jQuery libraries
+        // Should Assertion: does the assertion directly on the object itself
+        cy.get('#wooden_spoon')
+        .should('contain','Login')
+        .and('have.class','btn btn-primary');
+        // expect assertion: creates a subject of our test, then you implement
+        // different actions
+        cy.get('#wooden_spoon').then((buttonElement) => {
+            expect(buttonElement).to.have.text('Login');
+            expect(buttonElement).to.have.class('btn btn-primary');
+        })
+    })
+
 })
