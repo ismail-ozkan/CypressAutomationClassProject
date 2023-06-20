4c90cce85656e5734ba4e77605492c133f413ddb
ismailozkanlaw <ismailozkanlaw@gmail.com>
2023-06-20 07:34:56 +0300

    new locating ways

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -13,11 +13,25 @@ describe('Find or Get Elements by Using Different Locators', () => {
         // attribute name and value
         cy.get("[type='text']").clear(); // clear what is typed
 
+        // tagName
         // multiple element
         cy.get("input").each((item, index, list) => {
             // assert that the length of list is 2
             expect(list).to.have.length(2);
             expect(item).to.have.attr("type");
         })
+
+        // by attributeName
+        cy.get('[type]');
+
+        // by className
+        cy.get(".btn.btn-primary");
+
+        // by id
+        cy.get('#wooden_spoon');
+
+        // if I want to use text: no xpath in cypress, but it still possible with
+        // a different approach
+        cy.get('button').should('contain','Login').click();
     })
 })
