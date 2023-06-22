588ee99f07b97449559c44ec21b58ad6a477bb63
ismailozkanlaw <ismailozkanlaw@gmail.com>
2023-06-22 21:58:47 +0300

    add: new TC about checkboxes

--- cypress/e2e/inputs.cy.js
+++ cypress/e2e/inputs.cy.js
@@ -6,11 +6,11 @@ describe('Input Forms Tests', () => {
     cy.visit('/registration_form');
   });
 
-  it('Opening a web application', () => {
+  it.skip('Opening a web application', () => {
     // cy.visit('https://practice.cydeo.com');
     // cy.get('.list-group > :nth-child(1) > table').click();
   });
-  it('Check different input box fields and verify', () => {
+  it.skip('Check different input box fields and verify', () => {
     // fill the form for username
     cy.get('input[name="firstname"]').type('Mike');
     cy.get('input[name="lastname"]').type('Brown');
@@ -27,4 +27,43 @@ describe('Input Forms Tests', () => {
     cy.get('input[name="phone"]').type(phone);
     cy.get('input[name="birthday"]').type('01/01/1991');
   });
+
+  it.skip(('Check different radio button actions'), ()=> {
+    cy.get('.radio')
+    .find('[type=radio]')
+    .then((radio) => {
+      // get all radio buttons, select the first and verify that it is checked
+      cy.wrap(radio).first().check().should('be.checked');
+      /*
+it(('Check different radio button actions'), ()=> {: Bu satır, bir test senaryosunun başlangıcını gösterir. "Check different radio button actions" başlığı altında bir test senaryosu tanımlanır.
+cy.get('.radio'): Bu satır, Cypress'in .get() komutunu kullanarak .radio sınıfına sahip bir öğeyi seçer. Bu, radio düğmelerinin bir grup içinde olduğunu varsayar.
+.find('[tpye=radio]'): Bu satır, .get() komutunun döndürdüğü öğenin içinde, tpye=radio özniteliğine sahip olan tüm öğeleri seçer. Muhtemelen burada bir yazım hatası olmuş, çünkü özelliğin type olması gerekiyor (type=radio).
+.then((radio) => {: Bu satır, önceki .find() işlevi sonucunu bir parametre olarak alarak bir işlevi zincirler. Bu işlev, seçilen tüm radyo düğmelerini temsil eden radio parametresine erişim sağlar.
+cy.wrap(radio): Bu satır, radio parametresini bir Cypress nesnesine sarmalar. Böylece, sonraki Cypress komutlarını bu nesne üzerinde zincirleme şeklinde çalıştırabiliriz.
+.first(): Bu satır, radio öğeleri arasından ilkini seçer. İlk radyo düğmesini temsil eden bir Cypress nesnesi döndürür.
+.check(): Bu satır, seçilen radyo düğmesini işaretler. Yani, radyo düğmesini seçili hale getirir.
+.should('be.checked'): Bu satır, seçilen radyo düğmesinin işaretli olduğunu doğrular. Eğer radyo düğmesi işaretli değilse, bu ifade hata verecektir.
+*/      
+      /** 
+       * radio: is Jquery element, cy.wrap(radio) : turns in into Cypress Object so that I can use cypress functions
+       * first() : select first element
+       * check() : checks it out
+       * should : verifies whatever I provide as parameter 'be.checked'
+       */
+      // get all radio buttons, select the second and verify that it is checked and confirmation label is visible
+      cy.wrap(radio).eq(1).check().should('be.checked');
+      cy.get('[data-bv-icon-for="gender"]').should('be.visible');// common function used in tests
+      // Check the third is NOT checked
+      cy.wrap(radio).eq(2).should('not.be.checked');
+    })
+  });
+
+  it('Check different checkbox actions', () => {
+    // get all checkboxes, select JAVA and Verify
+    cy.get('[type="checkbox"]').then((checkbox) => {
+      cy.wrap(checkbox).eq(1).check().should('be.checked');
+      // uncheck JAVA
+      cy.wrap(checkbox).eq(1).uncheck().should('not.be.checked');
+    })
+  })
 });
