0cf5c65abe746f2da555a14745cc80c2511d97a6
ismailozkanlaw <ismailozkanlaw@gmail.com>
2023-06-28 08:33:45 +0300

    add: api testing files and TCs

--- cypress/e2e/pomTest/login.cy.js
+++ cypress/e2e/pomTest/login.cy.js
@@ -1,2 +1,49 @@
 import { auth } from '../../support/pages/auth';
 import { navigateTo } from '../../support/pages/navigation';
+
+const LoginLocators = require('../../support/pages/auth');// it's like a static element, we can access all things in the class in this way
+
+describe('Auth: Login User with Different Ways', () => {
+  // navigation to the test page
+  beforeEach('Navigate to login page', () => {
+    cy.clearAllCookies();
+    navigateTo.loginPage(); // this method we called it from our POM
+  });
+
+  it('Happy path scenario using POM FUNCTION', () => {
+    // auth.login("hard coding username", "hard coding password"); --> this is not a good way
+    cy.fixture('users').then((users) => {
+      auth.login(users.user2.username, users.user2.password);
+    });
+    // let's call our custom command to verify the text
+    cy.textExists('You logged into a secure area!');
+    auth.logout();
+  });
+  it('Happy path scenario using POM LOCATORS', () => {
+    // auth.login("hard coding username", "hard coding password"); --> this is not a good way
+    cy.fixture('users').then((users) => {
+      //auth.login(users.user2.username, users.user2.password);
+      // I need to import locators object
+      LoginLocators.locators.userName.type(users.user2.username);
+      LoginLocators.locators.password.type(users.user2.password);
+      LoginLocators.locators.submit.click();
+    });
+    // let's call our custom command to verify the text
+    cy.textExists('You logged into a secure area!');
+    auth.logout();
+  });
+
+  it('Happy path scenario using POM but NOT LOCATORS with auth import', () => {
+    // auth.login("hard coding username", "hard coding password"); --> this is not a good way
+    cy.fixture('users').then((users) => {
+      auth.loginWithLocatorsUsingValidCredentials();
+    });
+    // let's call our custom command to verify the text
+    cy.textExists('You logged into a secure area!');
+    auth.logout();
+  });
+  it('Check invalid user credentials', () => {
+    auth.login("invalidUserName123", "invalidPassword123");
+    cy.textExists('Your username is invalid!');
+  });
+});
--- cypress/support/pages/auth.js
+++ cypress/support/pages/auth.js
@@ -1,26 +1,40 @@
-class Auth{
-    // difference than java, class name does not have to be same with file name
-    // you can put more than one class in a file, and none of them have any superiority over each other.
-    login(user_name, password){
+class Auth {
+  // difference than java, class name does not have to be same with file name
+  // you can put more than one class in a file, and none of them have any superiority over each other.
+  login(user_name, password) {
     cy.get('[name="username"]').type(user_name);
     cy.get('[name="password"]').type(password);
     cy.get('#wooden_spoon').click();
-    }
+  }
 
-    logout(){
-        
-    }
+  loginWithLocatorsUsingValidCredentials(){
+    cy.fixture('users').then((users) => {
+      locators.userName.type(users.user2.username);
+      locators.password.type(users.user2.password);
+      locators.submit.click();
+    })
+  }
 
+  logout() {}
 }
-const auth = new Auth();  // object of the class we made
-
-
+const auth = new Auth(); // object of the class we made
 
 class Locators {
-
+    // we can create another class here, how do we apply findBy annotation of Selenium with Cypress
+    get userName() {
+      return cy.get('[name="username"]', {timeout: 10000});//defining custom timeout for a specific element, this timeout is a dynamic wait, when it finds it moves on
+    }
+    get password() {
+      return cy.get('[name="password"]');
+    }
+    get submit() {
+      return cy.get('#wooden_spoon');
+    }
 }
 
+const locators = new Locators();
 
 module.exports = {
-    auth
-}
+  auth,
+  locators
+};
